dba0a3b06310ed1b3608d8d20e85c1969e4887ee
RNLabs <silver2prod@gmail.com>
2021-05-07 05:27:49 +0300

    Previous work

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*let start = 3;
+$("#preloader-content").html(start);
+let a = setInterval(function() {
+    start--;
+    $("#preloader-content").html(start);
+    if (start == 0) {
+        $("html").removeClass("no-overflow");
+        $("#preloader").hide();
+        clearInterval(a);
+    }
+}, 1000);*/
